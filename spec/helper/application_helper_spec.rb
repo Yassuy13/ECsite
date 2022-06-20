c4b1bed21f5b69ac42e574d461d467478869001e
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-06-20 19:34:28 +0900

    空白の場合のテストを追加、環境変数の設定

--- spec/helper/application_helper_spec.rb
+++ spec/helper/application_helper_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe ApplicationHelper, type: :helper do
   it '商品を選択していない場合、タイトルがBIGBAG Storeのみ表示' do
     expect(full_title(nil)).to eq "BIGBAG Store"
   end
+
+  it '商品を選択していない場合、タイトルがBIGBAG Storeのみ表示' do
+    expect(full_title("")).to eq "BIGBAG Store"
+  end
 end
