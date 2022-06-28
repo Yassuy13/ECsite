12a49b8e922212d95434bd39a122dcea274d781b
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-06-28 21:26:36 +0900

    クラス名の追加、文言の変更、関連しない商品のテストの追加、サイドバーの画面遷移のテストの追加

--- spec/features/potepan/categories_spec.rb
+++ spec/features/potepan/categories_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature "Potepan::Categories", type: :feature do
   given(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
   given(:product) { create(:product, taxons: [taxon]) }
   given(:image) { build(:image) }
+  given(:shirts) { create(:taxon, name: "Shirts") }
   
   background do
     product.images << image
@@ -23,7 +24,7 @@ RSpec.feature "Potepan::Categories", type: :feature do
     expect(page).to have_selector("ul > li", text: taxon.products.count)
   end
   
-  scenario 'CategoriesまたはBrandをクリックしたらカテゴリー一覧が表示' do
+  scenario 'CategoriesまたはBrandのカテゴリー一覧を表示' do
     taxon.leaves.each do |category|
       expext(page).to have_content category.id
       expext(page).to have_content category.name
@@ -31,8 +32,25 @@ RSpec.feature "Potepan::Categories", type: :feature do
     end
   end
 
-  it "商品詳細ページへ" do
+  scenario "商品詳細ページへ" do
     click_link product.name
     expect(current_path).to eq potepan_product_path(product.id)
+  end
+
+  scenario '商品情報ページ及び関連しない商品が表示されないこと' do
+    within '.products_test' do
+      expect(page).to have_content product.name
+      expect(page).to have_content product.display_price
+      expect(page).not_to have_content shirts.name
+    end
+  end
+
+  scenario "サイドバーのカテゴリーをクリックしたら選択カテゴリー画面へ" do
+    within ".side-nav" do
+      expect(page).to have_content taxonomy.name
+      expect(page).to have_content "#{taxon.name} (#{taxon.products.count})"
+      click_on "#{taxon.name} (#{taxon.products.count})"
+      expect(current_path).to eq potepan_category_path(taxon.id)
+    end
   end
 end
