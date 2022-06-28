aac69f4107cd7d0605d8097cd958224e635d569f
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-06-29 00:16:28 +0900

    文言の統一、all?に変更、テストの修正

--- spec/features/potepan/categories_spec.rb
+++ spec/features/potepan/categories_spec.rb
@@ -5,18 +5,18 @@ RSpec.feature "Potepan::Categories", type: :feature do
   given(:taxon) { create(:taxon, taxonomy: taxonomy, parent: taxonomy.root) }
   given(:product) { create(:product, taxons: [taxon]) }
   given(:image) { build(:image) }
-  given(:shirts) { create(:taxon, name: "Shirts") }
+  given(:test_product) { create(:product) }
   
   background do
     product.images << image
     visit potepan_category_path(taxon.id)
   end
   
-  scenario 'タイトル表示がカテゴリー名に' do
+  scenario 'タイトル表示がカテゴリー名になること' do
     expect(page).to have_title "#{taxon.name} - BIGBAG Store"
   end
   
-  scenario '商品数を正しく表示' do
+  scenario '商品数を正しく表示されていること' do
     expect(page).to have_selector('.productBox', count: taxon.all_products.count)
   end
 
@@ -24,28 +24,28 @@ RSpec.feature "Potepan::Categories", type: :feature do
     expect(page).to have_selector("ul > li", text: taxon.products.count)
   end
   
-  scenario 'CategoriesまたはBrandのカテゴリー一覧を表示' do
-    taxon.leaves.each do |category|
+  scenario 'CategoriesまたはBrandのカテゴリー一覧を表示していること' do
+    taxon.leaves.all? do |category|
       expext(page).to have_content category.id
       expext(page).to have_content category.name
       expext(page).to have_content category.products.count
     end
   end
 
-  scenario "商品詳細ページへ" do
+  scenario "商品名から商品詳細ページへ遷移されること" do
     click_link product.name
     expect(current_path).to eq potepan_product_path(product.id)
   end
 
-  scenario '商品情報ページ及び関連しない商品が表示されないこと' do
+  it '関連しない商品が表示されないこと' do
     within '.products_test' do
       expect(page).to have_content product.name
       expect(page).to have_content product.display_price
-      expect(page).not_to have_content shirts.name
+      expect(page).not_to have_content test_product.name
     end
   end
 
-  scenario "サイドバーのカテゴリーをクリックしたら選択カテゴリー画面へ" do
+  scenario "サイドバーのカテゴリーをクリックしたら選択カテゴリー画面へ遷移されること" do
     within ".side-nav" do
       expect(page).to have_content taxonomy.name
       expect(page).to have_content "#{taxon.name} (#{taxon.products.count})"
--- spec/features/potepan/products_spec.rb
+++ spec/features/potepan/products_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Potepan::Products", type: :feature do
     visit potepan_product_path(product.id)
   end
   
-  scenario '商品詳細ページで商品名、価格、商品内容の表示' do
+  scenario '商品詳細ページで商品名、価格、商品内容の表示されること' do
     expect(page).to have_title "#{product.name} - BIGBAG Store"
     expect(page).to have_selector ".page-title h2", text: product.name
     expect(page).to have_selector ".active", text: product.name
@@ -18,7 +18,7 @@ RSpec.feature "Potepan::Products", type: :feature do
     expect(page).to have_selector ".media-body p", text: product.description
   end
   
-  scenario '一覧ページへ戻るをクリックしたら商品カテゴリーページへ移動' do
+  scenario '一覧ページへ戻るをクリックしたら商品カテゴリーページへ移動されること' do
     expect(page).to have_link '一覧ページへ戻る', href: potepan_category_path(product.taxons.first.id)
   end
 end
