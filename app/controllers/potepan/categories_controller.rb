1bbf9ed14074a4ca73b96d0f599f48234057d9a7
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-07-02 12:04:22 +0900

    コメントアウトの削除とファイル名の変更をしました。

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -2,6 +2,6 @@ class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
     @taxonomies = Spree::Taxonomy.all.includes(:root)
-    @products = @taxon.all_products.includes(master: [:images, :default_price])
+    @products = @taxon.all_products.includes(master: %i[images default_price])
   end
 end
