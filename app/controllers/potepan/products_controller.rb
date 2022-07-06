18dbcebf5490eb320f6dbf5eded5cff8568c9821
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-07-06 21:52:20 +0900

    ci上でrspecがエラーになる為、コードを１つ前に戻しました。

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -3,7 +3,6 @@
 module Potepan
   class ProductsController < ApplicationController
     RELATED_PRODUCTS_MAX_COUNT = 4
-
     def show
       @product = Spree::Product.find(params[:id])
       @related_products = @product.related_products.includes(master: [:images, :default_price]).limit(RELATED_PRODUCTS_MAX_COUNT)
