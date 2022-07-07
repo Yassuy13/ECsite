0f6c4816eecd7e85a60f28c9049685b5786649a9
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-07-07 21:51:47 +0900

    products_decoratorの空行の追加をしました。

--- app/models/potepan/product_decorator.rb
+++ app/models/potepan/product_decorator.rb
@@ -2,5 +2,6 @@ module Potepan::ProductDecorator
   def related_products
     Spree::Product.in_taxons(taxons).includes(master: [:images, :default_price]).where.not(id: id).distinct
   end
+
   Spree::Product.prepend self
 end
