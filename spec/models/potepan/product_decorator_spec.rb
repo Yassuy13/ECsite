eb5e849fe8779c62803a5a3d63dbc7b850db28ad
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-07-08 01:02:53 +0900

    specの内容を変更しました。

--- spec/models/potepan/product_decorator_spec.rb
+++ spec/models/potepan/product_decorator_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 RSpec.describe Potepan::ProductDecorator, type: :model do
   let(:taxonomy) { create(:taxonomy) }
   let(:taxon) { create(:taxon, taxonomy: taxonomy) }
+  let(:taxon2) { create(:taxon, taxonomy: taxonomy) }
+  let(:taxon3) { create(:taxon, taxonomy: taxonomy) }
   let(:product) { create(:product, taxons: [taxon]) }
   let!(:related_products) { create_list(:product, 5, taxons: [taxon]) }
+  let(:duplicate_related_products){ create(:product, taxons: [taxon2, taxon3]) }
 
   it "関連商品を取得できていること" do
     expect(product.related_products).to eq related_products
@@ -16,5 +19,9 @@ RSpec.describe Potepan::ProductDecorator, type: :model do
 
   it "表示される商品が重複しないこと" do
     expect(product.related_products).to eq related_products.uniq
+  end
+
+  it "複数のカテゴリーに属する商品が関連商品に含まれないこと" do
+    expect(product.related_products).not_to include duplicate_related_products
   end
 end
--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Potepan::Products', type: :request do
   describe 'GET /show' do
     let(:taxon) { create(:taxon) }
     let(:product) { create(:product, taxons: [taxon]) }
-    let(:related_product) { create_list(:product, 4, taxons: [taxon]) }
+    let(:related_product) { create_list(:product, 5, taxons: [taxon]) }
 
     before do
       get potepan_product_path(product.id)
@@ -22,12 +22,13 @@ RSpec.describe 'Potepan::Products', type: :request do
       expect(response.body).to include product.description
     end
 
-    it "関連商品名が含まれていること" do
+    it "関連商品を5件作成し、4件のみが取得されていること" do
       within ".related_test" do
         expect(response.body).to include related_product[0].name
         expect(response.body).to include related_product[1].name
         expect(response.body).to include related_product[2].name
         expect(response.body).to include related_product[3].name
+        expect(response.body).not_to include related_products[4].name
       end
     end
   end
