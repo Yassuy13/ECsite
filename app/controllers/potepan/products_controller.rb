7c31a233ea18d536a19d8622729c14da3a518aa1
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-07-08 12:13:28 +0900

    コントローラーの名前の変更とaltの変更をしました。

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -2,11 +2,11 @@
 
 module Potepan
   class ProductsController < ApplicationController
-    RELATED_PRODUCTS_MAX_COUNT = 4
+    MAX_PRODUCTS_COUNT = 4
 
     def show
       @product = Spree::Product.find(params[:id])
-      @related_products = @product.related_products.includes(master: [:images, :default_price]).limit(RELATED_PRODUCTS_MAX_COUNT)
+      @related_products = @product.related_products.includes(master: [:images, :default_price]).limit(MAX_PRODUCTS_COUNT)
     end
   end
 end
