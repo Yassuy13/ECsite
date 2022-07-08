ce490dca9102629caf795c281712297a11212f52
Yassuy13 <yassuy0413@yahoo.co.jp>
2022-07-08 11:14:11 +0900

    コードの変更とspecの内容を変更しました。

--- spec/models/potepan/product_decorator_spec.rb
+++ spec/models/potepan/product_decorator_spec.rb
@@ -6,8 +6,7 @@ RSpec.describe Potepan::ProductDecorator, type: :model do
   let(:taxon2) { create(:taxon, taxonomy: taxonomy) }
   let(:taxon3) { create(:taxon, taxonomy: taxonomy) }
   let(:product) { create(:product, taxons: [taxon]) }
-  let!(:related_products) { create_list(:product, 5, taxons: [taxon]) }
-  let(:duplicate_related_products){ create(:product, taxons: [taxon2, taxon3]) }
+  let!(:related_products) { create_list(:product, 5, taxons: [taxon, taxon2, taxon3]) }
 
   it "関連商品を取得できていること" do
     expect(product.related_products).to eq related_products
@@ -17,11 +16,7 @@ RSpec.describe Potepan::ProductDecorator, type: :model do
     expect(product.related_products).not_to include product
   end
 
-  it "表示される商品が重複しないこと" do
+  it "複数のカテゴリーに属する商品が表示される商品に重複しないこと" do
     expect(product.related_products).to eq related_products.uniq
-  end
-
-  it "複数のカテゴリーに属する商品が関連商品に含まれないこと" do
-    expect(product.related_products).not_to include duplicate_related_products
   end
 end
